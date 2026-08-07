41c89a10fa3a8998305db40841f0c711df25ad43
FranshuonSpotify <franhijo211008@gmail.com>
2026-08-07 12:41:15 +0200

    Traduce comentarios de normalizarTexto al español

--- db/consultas.php
+++ db/consultas.php
@@ -3984,12 +3984,12 @@ class Tcg
 	// el match (el JSON oficial mezcla "Aire"/"aire", "Montaña"/"montaña"...).
 	public function normalizarTexto(string $s): string {
 		$s = trim(mb_strtolower($s, 'UTF-8'));
-		// Replace common Spanish characters manually to avoid iconv translit issues on some systems
+		// Reemplaza caracteres españoles comunes manualmente para evitar problemas de translit en iconv en algunos sistemas
 		$s = str_replace(['ñ', 'á', 'é', 'í', 'ó', 'ú', 'à', 'è', 'ì', 'ò', 'ù'],
 		                   ['n', 'a', 'e', 'i', 'o', 'u', 'a', 'e', 'i', 'o', 'u'], $s);
 		$translit = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $s);
 		if ($translit !== false) { $s = $translit; }
-		// Remove any remaining special characters that iconv might have left
+		// Elimina caracteres especiales restantes que iconv pudiera haber dejado
 		$s = preg_replace('/[^a-z0-9\s]/i', '', $s);
 		return preg_replace('/\s+/', ' ', $s);
 	}
